985bb37337bd81d1187e5462ca870206a0009277
Данил <ddrxg@MacBook-Air-Danil.local>
2025-08-19 22:17:29 +0300

    задана начальная структура проекта

---
+++ app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from src.db.base import base, engine
+    
+app = FastAPI(title="Task Manager API", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+async def read_root():
+    return {"message": "status: ok"}
+
+@app.on_event("startup")
+async def startup_event():
+    base.metadata.create_all(bind=engine)
+
+@app.on_event("shutdown")
+def shutdown_event():
+    with open("log.txt", mode="a") as log:
+        log.write("Application shutdown")
